8565efd37c48cf50ce3e82a5851fda91045bff93
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-04-04 14:25:13 +0200

    feat: Adds new Card functionality

--- screens/AddNewCardView.js
+++ screens/AddNewCardView.js
@@ -1,16 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// Actions
+import { addNewCard } from '../store/actions/Deck';
 
 // UI
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { Card, Form, Button } from 'native-base';
 
 class AddNewCardView extends React.Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  // Event Listeners
+  handleButtonPress() {
+    // Add Card to Deck
+    const deckId = this.props.navigation.getParam('deckId')
+
+    this.props.dispatch(addNewCard(deckId, {
+      question: this.state.question,
+      answer: this.state.answer
+    }))
+
+    // Redirect to SingleDeckView
+    this.props.navigation.navigate('SingleDeck', { deckId })
+  }
+
   render() {
     return (
       <View>
-        <Text>NEW CARD VIEW</Text>
+        <Card>
+          <Form>
+            <TextInput
+              onChangeText={question => this.setState({ question })}
+              fieldLabel='Question'
+              placeholder='I.e. "How tall is the Eiffel Tower"'
+              maxLength={50}
+            />
+            <TextInput
+              onChangeText={answer => this.setState({ answer })}
+              fieldLabel='Answer'
+              placeholder='I.e. "300 meters"'
+              maxLength={50}
+            />
+            <Button onPress={this.handleButtonPress.bind(this)}>
+              <Text>Submit Title</Text>
+            </Button>
+          </Form>
+        </Card>
       </View>
     )
   }
 }
 
-export default AddNewCardView;
+export default connect()(AddNewCardView);
